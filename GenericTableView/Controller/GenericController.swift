d5b3417af6ff45070d7b07facc6d3fcfad318314
omrobbie <putu.roby@yahoo.com>
2020-03-10 11:49:04 +0800

    add title to generic with auto layout constraint

--- GenericTableView/Controller/GenericController.swift
+++ GenericTableView/Controller/GenericController.swift
@@ -12,8 +12,29 @@ class GenericController: UIViewController {
 
     var tableView: GenericTableView<Person, ReuseableCell>!
 
+    var txtTitle: UILabel = {
+        let lbl = UILabel()
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        lbl.backgroundColor = .lightGray
+        lbl.textColor = .white
+        lbl.textAlignment = .center
+        lbl.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+        lbl.text = "Generic"
+
+        return lbl
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        view.addSubview(txtTitle)
+        NSLayoutConstraint.activate([
+            txtTitle.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            txtTitle.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            txtTitle.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            txtTitle.heightAnchor.constraint(equalToConstant: 64)
+        ])
+
         tableView = GenericTableView(frame: view.frame, items: Person.persons, setupCell: { (item, cell) in
             cell.txtFirstName.text = item.firstName
             cell.txtLastName.text = item.lastName
@@ -22,6 +43,13 @@ class GenericController: UIViewController {
             alertMessage(message: message, sender: self)
         })
 
+        tableView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(tableView)
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: txtTitle.bottomAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor)
+        ])
     }
 }
